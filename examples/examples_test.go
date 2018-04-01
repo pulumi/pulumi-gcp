9a6c36cb9eb0471277eaf08120476c1d824a0329
Subha Bhattacharyay <subhabh@outlook.com>
2018-04-01 12:12:38 -0700

    Updating examples_test

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -14,26 +14,19 @@ import (
 )
 
 func TestExamples(t *testing.T) {
-	keyFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-	if keyFile == "" {
-		t.Skipf("Skipping test due to missing GOOGLE_APPLICATION_CREDENTIALS variable")
-	}
-	fmt.Println(keyFile);
+	
 	project:= os.Getenv("GOOGLE_PROJECT");
 	if project == "" {
 		t.Skipf("Skipping test due to missing GOOGLE_PROJECT variable")
 	}
-	fmt.Println(project);
 	region := os.Getenv("GOOGLE_REGION");
 	if region == "" {
 		t.Skipf("Skipping test due to missing GOOGLE_REGION variable")
 	}
-	fmt.Println(region);
 	zone := os.Getenv("GOOGLE_ZONE");
 	if zone == "" {
 		t.Skipf("Skipping test due to missing GOOGLE_ZONE variable")
 	}
-	fmt.Println(zone);
 	cwd, err := os.Getwd()
 	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
 		return
@@ -42,7 +35,6 @@ func TestExamples(t *testing.T) {
 	// base options shared amongst all tests.
 	base := integration.ProgramTestOptions{
 		Config: map[string]string{
-			"gcp:config:credentials": keyFile,
 			"gcp:config:project": project,
 			"gcp:config:zone": zone,
 			"gcp:config:region": region,
