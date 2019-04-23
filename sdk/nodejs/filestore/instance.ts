34ffc0df6f402dcd44fce1d9f40f050f19641f52
Justin Van Patten <jvp@justinvp.com>
2019-04-23 09:28:08 -0700

    Strip "Open in Cloud Shell" buttons
    Use the latest `pulumi-terraform`, which strips "Open in Cloud Shell"
    buttons from doc comments.

--- sdk/nodejs/filestore/instance.ts
+++ sdk/nodejs/filestore/instance.ts
@@ -17,12 +17,6 @@ import * as utilities from "../utilities";
  *     * [Official Documentation](https://cloud.google.com/filestore/docs/creating-instances)
  *     * [Use with Kubernetes](https://cloud.google.com/filestore/docs/accessing-fileshares)
  *     * [Copying Data In/Out](https://cloud.google.com/filestore/docs/copying-data)
- * 
- * <div class = "oics-button" style="float: right; margin: 0 0 -15px">
- *   <a href="https://console.cloud.google.com/cloudshell/open?cloudshell_git_repo=https%3A%2F%2Fgithub.com%2Fterraform-google-modules%2Fdocs-examples.git&cloudshell_working_dir=filestore_instance_basic&cloudshell_image=gcr.io%2Fgraphite-cloud-shell-images%2Fterraform%3Alatest&open_in_editor=main.tf&cloudshell_print=.%2Fmotd&cloudshell_tutorial=.%2Ftutorial.md" target="_blank">
- *     <img alt="Open in Cloud Shell" src="//gstatic.com/cloudssh/images/open-btn.svg" style="max-height: 44px; margin: 32px auto; max-width: 100%;">
- *   </a>
- * </div>
  */
 export class Instance extends pulumi.CustomResource {
     /**
