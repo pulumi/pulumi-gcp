1e66adc28216d463a986d2d947a7ccb36c92009b
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-07-16 02:47:25 -0700

    version

--- resources.go
+++ resources.go
@@ -1214,7 +1214,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi":    "^0.17.8",
+				"@pulumi/pulumi":    "=0.17.24-dev.1563304341",
 				"read-package-json": "^2.0.13",
 				"@types/express":    "^4.16.0",
 			},
