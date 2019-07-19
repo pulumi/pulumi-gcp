477c9d90dd54f9c41c02d91cfef07daeba715d56
Justin Van Patten <jvp@justinvp.com>
2019-07-19 10:58:37 -0700

    `terraform-provider-gcp` => `terraform-provider-google`
    When we originally generated these attributions, we assumed the Pulumi package and Terraform Provider had the same name, but that's not the case for GCP.

--- sdk/go/gcp/endpoints/endpoints.go
+++ sdk/go/gcp/endpoints/endpoints.go
@@ -1,8 +1,8 @@
 //nolint: lll
 // Package endpoints exports types, functions, subpackages for provisioning endpoints resources.
 //
-// > This provider is a derived work of the [Terraform Provider](https://github.com/terraform-providers/terraform-provider-gcp)
+// > This provider is a derived work of the [Terraform Provider](https://github.com/terraform-providers/terraform-provider-google)
 // > distributed under [MPL 2.0](https://www.mozilla.org/en-US/MPL/2.0/). If you encounter a bug or missing feature,
 // > first check the [`pulumi/pulumi-gcp` repo](https://github.com/pulumi/pulumi-gcp/issues); however, if that doesn't turn up anything,
-// > please consult the source [`terraform-providers/terraform-provider-gcp` repo](https://github.com/terraform-providers/terraform-provider-gcp/issues).
+// > please consult the source [`terraform-providers/terraform-provider-google` repo](https://github.com/terraform-providers/terraform-provider-google/issues).
 package endpoints
