9b2b2297d619b74a53bed635fc37e2e92ffa1daa
Mikhail Shilkov <shilkov@gmail.com>
2019-12-02 18:59:16 +0100

    Undo name change

--- sdk/dotnet/Utilities.cs
+++ sdk/dotnet/Utilities.cs
@@ -66,7 +66,7 @@ namespace Pulumi.Gcp
         static Utilities()
         {
             var assembly = typeof(Utilities).GetTypeInfo().Assembly;
-            using var stream = assembly.GetManifestResourceStream("Pulumi.Gc[].version.txt");
+            using var stream = assembly.GetManifestResourceStream("Pulumi.Gcp.version.txt");
             using var reader = new StreamReader(stream ?? throw new NotSupportedException("Missing embedded version.txt file"));
             version = reader.ReadToEnd().Trim();
         }
