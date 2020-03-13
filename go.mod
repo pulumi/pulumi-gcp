ce15de6b6b3086a7d47b149fa565ca6859073690
Mikhail Shilkov <github@mikhail.io>
2020-03-13 16:49:14 +0100

    Fix python docs and prepare the release

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/hashicorp/terraform-plugin-sdk v1.4.0
 	github.com/pkg/errors v0.8.1
-	github.com/pulumi/pulumi v1.12.1
+	github.com/pulumi/pulumi v1.12.2-0.20200313044354-8111d33438b9
 	github.com/pulumi/pulumi-terraform-bridge v1.8.2
 	github.com/stretchr/testify v1.4.1-0.20191106224347-f1bd0923b832
 	github.com/terraform-providers/terraform-provider-google-beta v0.0.0-00010101000000-000000000000
--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -30,12 +30,7 @@ def readme():
 
 setup(name='pulumi_gcp',
       version='${VERSION}',
-      description="""A Pulumi package for creating and managing Google Cloud Platform resources.
-
-> This provider is a derived work of the [Terraform Provider](https://github.com/terraform-providers/terraform-provider-google-beta)
-> distributed under [MPL 2.0](https://www.mozilla.org/en-US/MPL/2.0/). If you encounter a bug or missing feature,
-> first check the [`pulumi/pulumi-gcp` repo](https://github.com/pulumi/pulumi-gcp/issues); however, if that doesn't turn up anything,
-> please consult the source [`terraform-providers/terraform-provider-google-beta` repo](https://github.com/terraform-providers/terraform-provider-google-beta/issues).""",
+      description="A Pulumi package for creating and managing Google Cloud Platform resources.",
       long_description=readme(),
       long_description_content_type='text/markdown',
       cmdclass={
