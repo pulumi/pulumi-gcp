b700ca1959b0ab937941d7340714c0dd2d464764
Jakub Sacha <jakubsacha@gmail.com>
2022-07-09 19:38:15 +0200

    Update secret version example
    Hey,
    
    There is a little bug in the example, that I'd like to fix. This file is autogenerated, but I struggle to find the source of it. Could you point me place where this change should be applied?

--- sdk/go/gcp/secretmanager/secretVersion.go
+++ sdk/go/gcp/secretmanager/secretVersion.go
@@ -29,7 +29,7 @@ import (
 //
 // func main() {
 // 	pulumi.Run(func(ctx *pulumi.Context) error {
-// 		_, err := secretmanager.NewSecret(ctx, "secret-basic", &secretmanager.SecretArgs{
+// 		secret_basic, err := secretmanager.NewSecret(ctx, "secret-basic", &secretmanager.SecretArgs{
 // 			SecretId: pulumi.String("secret-version"),
 // 			Labels: pulumi.StringMap{
 // 				"label": pulumi.String("my-label"),
