c34c949b4e4cccff4fdc3a93ca6f4b89e50d5aea
Guinevere Saenger <guinevere@pulumi.com>
2023-10-09 14:16:53 -0700

    Fix Go files for dirty worktree

--- sdk/go/gcp/dataflow/job.go
+++ sdk/go/gcp/dataflow/job.go
@@ -138,7 +138,7 @@ import (
 //			}
 //			_, err := random.NewRandomId(ctx, "bigDataJobNameSuffix", &random.RandomIdArgs{
 //				ByteLength: pulumi.Int(4),
-//				Keepers: pulumi.StringMap{
+//				Keepers: pulumi.AnyMap{
 //					"region":          pulumi.Any(_var.Region),
 //					"subscription_id": pulumi.String(bigDataJobSubscriptionId),
 //				},
