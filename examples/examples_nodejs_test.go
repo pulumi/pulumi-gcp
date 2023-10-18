bddab320a44bc8ed6eb64c76b3bae49f4d6324c2
Ramon Quitales <ramon@pulumi.com>
2023-10-18 08:44:29 -0700

    Re-enable TestAccBucket (#1276)
    Fixes: #1267
    
    Note: This re-enables the test with retry on failure. It would be ideal
    if we could handle retry on the provider/resource level instead so that
    users do not need to re-run `pulumi up` to fix this.

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -72,6 +72,9 @@ func TestAccBucket(t *testing.T) {
 			// One change is known to occur during refresh of the resources in this example:
 			// * `~  gcp:storage:Bucket f-bucket updated changes: + websites`
 			ExpectRefreshChanges: true,
+			// GCP buckets seem to be eventually consistent, so we need to retry on failure when deploying a cloud function
+			// that uses the bucket as a trigger.
+			RetryFailedSteps: true,
 		})
 
 	integration.ProgramTest(t, &test)
