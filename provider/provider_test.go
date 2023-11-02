aef6b80d448442c14a84fbf90c7b4d0a6e6e2c12
Anton Tayanovskyy <anton@pulumi.com>
2023-11-02 14:51:35 -0400

    Ensure CI runs PreviewOnly upgrade tests (#1303)
    Fixes https://github.com/pulumi/pulumi-gcp/issues/1259
    
    Upgrade tests in PreviewOnly mode, as well as E2E tests over the same
    test programs, now run in CI. See
    [example](https://github.com/pulumi/pulumi-gcp/actions/runs/6727319213/job/18285119414?pr=1303#step:25:119).
    
    - This is accomplished by using .ci-mgmt.yml and `make ci-mgmt` to
    extend the test matrix to run ./provider tests first, before it runs
    ./examples tests. The test matrix is split by language, therefore the
    provider tests are now also split, yaml and java languages are currently
    grouped into one bucket.
    
    - The tests are run only in the test matrix, and early tests are skipped
    in prerequisites - as currently no tests can succeed without Google
    credentials, and prerequisites is still lacking those credentials
    
    - Negated build tags are used to split the tests in CI while still
    making them run locally without setting build tags
    
    - Baselines are re-recorded on CI using the CI-native region, project
    and zone
    
    - To enable running PreviewOnly tests locally, ignoreChanges workaround
    is added to every program to disregard differences in region, project,
    and zone between CI-recorded baselines and local pulumi-development
    project

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -4,134 +4,56 @@ package gcp
 
 import (
 	"context"
-	"fmt"
-	"path/filepath"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/pulumi/providertest"
-	"github.com/pulumi/providertest/flags"
 	"github.com/pulumi/pulumi-gcp/provider/v7/pkg/version"
 	pfbridge "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 )
-
-func TestDNSRecordSet(t *testing.T) {
-	runTest(t, test(t, "test-programs/dns-recordset"))
-}
-
-func TestPubSubSubscription(t *testing.T) {
-	runTest(t, test(t, "test-programs/pubsub-subscription"))
-}
-
-func TestPubSubTopic(t *testing.T) {
-	runTest(t, test(t, "test-programs/pubsub-topic"))
-}
-
-func TestServiceAccount(t *testing.T) {
-	runTest(t, test(t, "test-programs/serviceaccount-account",
-		providertest.WithSkippedUpgradeTestMode(providertest.UpgradeTestMode_PreviewOnly,
-			"TODO[pulumi/providertest#7] PreviewOnly is confused about stack names"),
-	))
-}
-
-func TestStorageBucket(t *testing.T) {
-	runTest(t, test(t, "test-programs/storage-bucket"))
-}
-
-func TestStorageBucketObject(t *testing.T) {
-	t.Skipf("TODO[pulumi/providertest#2] skipping because Assets are not working yet")
-	runTest(t, test(t, "test-programs/storage-bucketobject"))
-}
-
-func TestSecretManagerSecret(t *testing.T) {
-	runTest(t, test(t, "test-programs/secretmanager-secret"))
-}
-
-func TestSqlUser(t *testing.T) {
-	runTest(t, test(t, "test-programs/sql-user"))
-}
-
-func TestBigQueryTable(t *testing.T) {
-	runTest(t, test(t, "test-programs/bigquery-table",
-		providertest.WithSkippedUpgradeTestMode(providertest.UpgradeTestMode_PreviewOnly,
-			"TODO[pulumi/providertest#7] PreviewOnly is confused about stack names"),
-	))
-}
-
-func TestComputeFirewall(t *testing.T) {
-	runTest(t, test(t, "test-programs/compute-firewall"))
-}
-
-func TestCloudFunction(t *testing.T) {
-	t.Skipf("TODO[pulumi/providertest#2] skipping because Assets are not working yet")
-	runTest(t, test(t, "test-programs/cloudfunctions-function"))
-}
-
-// Test programs that were automatically extracted from examples without autocorrection.
-func TestAutoExtractedPrograms(t *testing.T) {
-	type testCase struct {
-		program string
-	}
-
-	testCases := []testCase{
-		{"monitoring-alertpolicy-1"},
-		{"bigquery-datasetaccess-3"},
-		{"bigquery-routine-1"},
-		{"bigquery-routine-2"},
-		{"cloudrunv2-job-6"},
-		{"cloudrunv2-service-4"},
-		{"cloudrunv2-service-5"},
-		{"cloudrunv2-service-7"},
-		{"compute-disk-1"},
-		{"compute-disk-3"},
-	}
-
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.program, func(t *testing.T) {
-			runTest(t, test(t, filepath.Join("test-programs", tc.program)))
-		})
-	}
-}
 
 func TestUpgradeCoverage(t *testing.T) {
 	providertest.ReportUpgradeCoverage(t)
 }
 
 func test(t *testing.T, dir string, opts ...providertest.Option) *providertest.ProviderTest {
+	envVars := map[string]string{
+		"gcp:config:project": "GOOGLE_PROJECT",
+		"gcp:config:zone":    "GOOGLE_ZONE",
+		"gcp:config:region":  "GOOGLE_REGION",
+	}
+
+	defaultValues := map[string]string{
+		"gcp:config:project": "pulumi-development",
+	}
+
+	for setting, envVar := range envVars {
+		if envVarValue, ok := os.LookupEnv(envVar); ok {
+			opts = append(opts, providertest.WithConfig(setting, envVarValue))
+		} else if defValue, ok := defaultValues[setting]; ok {
+			opts = append(opts, providertest.WithConfig(setting, defValue))
+		}
+	}
+
 	opts = append(opts,
 		providertest.WithProviderName("gcp"),
 		providertest.WithBaselineVersion("6.67.0"),
-		providertest.WithConfig("gcp:project", "pulumi-development"),
-		providertest.WithResourceProviderServer(providerServer(t)))
+		providertest.WithResourceProviderServer(providerServer(t)),
+		providertest.WithSkippedUpgradeTestMode(providertest.UpgradeTestMode_Quick,
+			"TODO[pulumi/providertest#28] Skipping Quick mode because of panics and implicit dependency on Configure"),
+	)
+
 	return providertest.NewProviderTest(dir, opts...)
 }
 
-// This funcion inlines bits of ProviderTest.Run to avoid running E2E tests and instead focuses on
-// upgrade tests. Currently E2E tests are not able to run in this provider.
-//
-// TODO[pulumi/ci-mgmt#675] expose credentials so this just becomes pt.Run(t).
 func runTest(t *testing.T, pt *providertest.ProviderTest) {
-	t.Run("upgrade-snapshot", func(t *testing.T) {
-		t.Helper()
-		if flags.Snapshot.IsSet() {
-			t.Logf("Recording baseline behavior because %s", flags.Snapshot.WhySet())
-			pt.VerifyUpgradeSnapshot(t)
-		} else {
-			t.Skipf("Skip recording baseline behavior because %s", flags.Snapshot.WhyNotSet())
-		}
-	})
-	for _, m := range providertest.UpgradeTestModes() {
-		t.Run(fmt.Sprintf("upgrade-%s", m), func(t *testing.T) {
-			t.Helper()
-			if m == providertest.UpgradeTestMode_Quick {
-				t.Skip("TODO[pulumi/providertest#28] Skipping Quick mode because of panics and implicit dependency on Configure")
-			}
-			pt.VerifyUpgrade(t, m)
-		})
+	if testing.Short() {
+		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without GCP creds")
 	}
+	pt.Run(t)
 }
 
 func providerServer(t *testing.T) pulumirpc.ResourceProviderServer {
