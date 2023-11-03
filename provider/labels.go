32a0b70b68aa53230168193e3893a15c92edab92
Guinevere Saenger <guinevere@pulumi.com>
2023-11-03 13:04:33 -0700

    Fix permanent diff on Pulumi Labels via state transform function

--- provider/labels.go
+++ provider/labels.go
@@ -3,8 +3,10 @@
 package gcp
 
 import (
+	"context"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 )
 
 // GCP has many resources with labels called "terraform_labels". We want to change those
@@ -15,10 +17,16 @@ import (
 // tfbridge.SchemaInfo.
 func fixLabelNames(prov *tfbridge.ProviderInfo) {
 	// Recursively applies the label fix
-	var apply func(string, shim.Schema, *tfbridge.SchemaInfo)
-	apply = func(key string, m shim.Schema, info *tfbridge.SchemaInfo) {
+	var apply func(string, shim.Schema, *tfbridge.SchemaInfo, *[]resource.PropertyPath, resource.PropertyPath)
+
+	apply = func(key string, m shim.Schema, info *tfbridge.SchemaInfo,
+		paths *[]resource.PropertyPath,
+		currentPath resource.PropertyPath,
+	) {
 		if key == "terraform_labels" {
 			info.Name = "pulumiLabels"
+			// Gather all paths to pulumiLabels
+			*paths = append(*paths, currentPath)
 		}
 
 		if m == nil {
@@ -26,7 +34,7 @@ func fixLabelNames(prov *tfbridge.ProviderInfo) {
 		}
 
 		switch elem := m.Elem().(type) {
-		case shim.Resource:
+		case shim.Resource: // this is a nested object
 			obj := elem.Schema()
 			if info.Elem == nil {
 				info.Elem = new(tfbridge.SchemaInfo)
@@ -36,45 +44,78 @@ func fixLabelNames(prov *tfbridge.ProviderInfo) {
 				info.Elem.Fields = make(map[string]*tfbridge.SchemaInfo, obj.Len())
 			}
 			obj.Range(func(key string, m shim.Schema) bool {
-				i, ok := info.Fields[key]
+				i, ok := info.Elem.Fields[key]
 				if !ok {
 					i = new(tfbridge.SchemaInfo)
 					info.Elem.Fields[key] = i
 				}
-				apply(key, m, i)
+				currentPath = append(currentPath, key)
+				apply(key, m, i, paths, currentPath)
 				return true
 			})
-		case shim.Schema:
+		case shim.Schema: // this is a map or array or a Set
 			if info.Elem == nil {
 				info.Elem = new(tfbridge.SchemaInfo)
 			}
-			apply("", elem, info.Elem)
+			currentPath = append(currentPath, "*")
+			apply("", elem, info.Elem, paths, currentPath)
 		}
 	}
+	// update takes a terraform schema map and a pulumi schema info map of a given resource, and calls `apply` to do
+	// necessary resource transformations.
+	// - Rename terraformLabels to pulumiLabels at schema generation time
+	// - Create a collection of resource property paths that have this pulumiLabels key amd return it.
+	update := func(s shim.SchemaMap, fields *map[string]*tfbridge.SchemaInfo) []resource.PropertyPath {
+		var paths []resource.PropertyPath
 
-	update := func(s shim.SchemaMap, fields *map[string]*tfbridge.SchemaInfo) {
 		if *fields == nil {
 			*fields = make(map[string]*tfbridge.SchemaInfo, s.Len())
 		}
 		s.Range(func(key string, m shim.Schema) bool {
+			currentPath := resource.PropertyPath{key}
 			i, ok := (*fields)[key]
 			if !ok {
 				i = new(tfbridge.SchemaInfo)
 				(*fields)[key] = i
 			}
-			apply(key, m, i)
+			apply(key, m, i, &paths, currentPath)
 			return true
 		})
+		return paths
 	}
 
 	rMap := prov.P.ResourcesMap()
 	for token, info := range prov.Resources {
-		update(rMap.Get(token).Schema(), &info.Fields)
+		// Rename pulumiLabel fields and obtain paths
+		labelPaths := update(rMap.Get(token).Schema(), &info.Fields)
+		if len(labelPaths) > 0 {
+			info.TransformFromState = ensureLabelPathsExist(labelPaths)
+		}
 	}
 
 	dMap := prov.P.DataSourcesMap()
 	for token, info := range prov.DataSources {
 		update(dMap.Get(token).Schema(), &info.Fields)
+	}
+
+}
+
+// pulumiLabels represents a field added upstream via a custom diff, as terraform_labels.
+// There is a bug: f this field is nil in the Pulumi state, it will never be added to the state,
+// creating a permanent diff.
+// See also: https://github.com/pulumi/pulumi-gcp/issues/1314
+func ensureLabelPathsExist(paths []resource.PropertyPath) tfbridge.PropertyTransform {
+
+	return func(ctx context.Context, prop resource.PropertyMap) (resource.PropertyMap, error) {
+		obj := resource.NewObjectProperty(prop)
+		for _, path := range paths {
+			// If a `pulumiLabels` field is not set at the expected path, we will set it to an empty property Map,
+			// so Diff can detect it.
+			if _, found := path.Get(obj); !found {
+				path.Set(obj, resource.NewObjectProperty(resource.PropertyMap{}))
+			}
+		}
+		return obj.ObjectValue(), nil
 	}
 
 }
