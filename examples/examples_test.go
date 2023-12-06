6bd87777d714ec1ab811e24ef6a5763af8143dd1
Venelin <venelin@pulumi.com>
2023-12-06 15:51:26 +0000

    test for the bool label issue

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -15,6 +15,7 @@
 package examples
 
 import (
+	"context"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -22,7 +23,11 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	gcp "github.com/pulumi/pulumi-gcp/provider/v7"
+	pfbridge "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
+	testutils "github.com/pulumi/pulumi-terraform-bridge/testing/x"
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 )
 
@@ -102,4 +107,117 @@ func validateAPITest(isValid func(body string)) func(t *testing.T, stack integra
 		assert.NoError(t, err)
 		isValid(string(body))
 	}
+}
+
+func TestBucketBooleanLabel(t *testing.T) {
+	replay(t, `[
+	{
+		"method": "/pulumirpc.ResourceProvider/Configure",
+		"request": {
+			"variables": {
+				"gcp:config:project": "pulumi-development"
+			},
+			"args": {
+				"project": "pulumi-development",
+				"version": "7.2.1"
+			},
+			"acceptSecrets": true,
+			"acceptResources": true,
+			"sendsOldInputs": true,
+			"sendsOldInputsToDelete": true
+		},
+		"response": {
+			"supportsPreview": true
+		},
+		"metadata": {
+			"kind": "resource",
+			"mode": "client",
+			"name": "gcp"
+		}
+	},
+	{
+		"method": "/pulumirpc.ResourceProvider/Diff",
+		"request": {
+			"id": "my-bucket-409ae4e",
+			"urn": "urn:pulumi:dev::gcp_bucket_python::gcp:storage/bucket:Bucket::my-bucket",
+			"olds": {
+				"__meta": "{\"e2bfb730-ecaa-11e6-8f88-34363bc7c4c0\":{\"create\":600000000000,\"read\":240000000000,\"update\":240000000000}}",
+				"autoclass": null,
+				"cors": [],
+				"customPlacementConfig": null,
+				"defaultEventBasedHold": false,
+				"encryption": null,
+				"forceDestroy": false,
+				"id": "my-bucket-409ae4e",
+				"labels": {
+					"bad-bool": "1"
+				},
+				"lifecycleRules": [],
+				"location": "US",
+				"logging": null,
+				"name": "my-bucket-409ae4e",
+				"project": "pulumi-development",
+				"publicAccessPrevention": "inherited",
+				"requesterPays": false,
+				"retentionPolicy": null,
+				"selfLink": "https://www.googleapis.com/storage/v1/b/my-bucket-409ae4e",
+				"storageClass": "STANDARD",
+				"uniformBucketLevelAccess": false,
+				"url": "gs://my-bucket-409ae4e",
+				"versioning": null,
+				"website": null
+			},
+			"news": {
+				"__defaults": [
+					"forceDestroy",
+					"name",
+					"storageClass"
+				],
+				"forceDestroy": false,
+				"labels": {
+					"__defaults": [],
+					"bad-bool": true
+				},
+				"location": "US",
+				"name": "my-bucket-409ae4e",
+				"storageClass": "STANDARD"
+			},
+			"oldInputs": {
+				"__defaults": [
+					"forceDestroy",
+					"name",
+					"storageClass"
+				],
+				"forceDestroy": false,
+				"labels": {
+					"__defaults": [],
+					"bad-bool": true
+				},
+				"location": "US",
+				"name": "my-bucket-409ae4e",
+				"storageClass": "STANDARD"
+			}
+		},
+		"metadata": {
+			"kind": "resource",
+			"mode": "client",
+			"name": "gcp"
+		}
+	}
+]`)
+}
+
+func replay(t *testing.T, sequence string) {
+	info := gcp.Provider()
+	ctx := context.Background()
+	p, err := pfbridge.MakeMuxedServer(ctx, info.Name, info,
+		/*
+		 * We leave the schema blank. This will result in incorrect calls to
+		 * GetSchema, but otherwise does not effect the provider. It reduces the
+		 * time to test start by minutes.
+		 */
+		[]byte("{}"),
+	)(nil)
+	require.NoError(t, err)
+	testutils.ReplaySequence(t, p, sequence)
 }
