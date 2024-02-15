1bf5c15df59cde147b0675176f1619fb7ac0fa9e
Venelin <venelin@pulumi.com>
2024-02-15 15:16:13 +0000

    fix short/long mode test

--- provider/provider_yaml_test.go
+++ provider/provider_yaml_test.go
@@ -133,8 +133,8 @@ func TestWrongRegionWarning(t *testing.T) {
 }
 
 func TestNoGlobalProjectWarning(t *testing.T) {
-	if !testing.Short() {
-		t.Skip("Only run in short mode, since we want to NOT have credentials.")
+	if testing.Short() {
+		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without GCP creds")
 	}
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
