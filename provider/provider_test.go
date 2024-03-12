6ce1430c6b0f35d008b1f3b290d48d2bdbca71a3
Ian Wahbe <ian@wahbe.com>
2024-03-12 16:14:44 +0100

    Add replay test to prevent #1794 regressing

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/pulumi/providertest/pulumitest"
 	"github.com/pulumi/providertest/pulumitest/assertpreview"
 	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/pulumi/providertest/replay"
 	pfbridge "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 
@@ -72,4 +73,270 @@ func providerServer(t *testing.T) pulumirpc.ResourceProviderServer {
 	)(nil)
 	require.NoError(t, err)
 	return p
+}
+
+func TestRepro1794(t *testing.T) {
+	replay.ReplaySequence(t, providerServer(t), `
+[
+    {
+        "method": "/pulumirpc.ResourceProvider/Configure",
+        "request": {
+            "variables": {
+                "gcp:config:defaultLabels": "{\"hi\":\"value\"}",
+                "gcp:config:project": "pulumi-development",
+                "gcp:config:region": "us-west1"
+            },
+            "args": {
+                "defaultLabels": "{\"hi\":\"value\"}",
+                "project": "pulumi-development",
+                "region": "us-west1",
+                "version": "6.67.0"
+            },
+            "acceptSecrets": true,
+            "acceptResources": true,
+            "sendsOldInputs": true,
+            "sendsOldInputsToDelete": true
+        },
+        "response": {
+            "supportsPreview": true
+        },
+        "metadata": {
+            "kind": "resource",
+            "mode": "client",
+            "name": "gcp"
+        }
+    },
+    {
+        "method": "/pulumirpc.ResourceProvider/Diff",
+        "request": {
+            "id": "locations/us-central1/namespaces/pulumi-development/services/b-476c751",
+            "urn": "urn:pulumi:dev::dev-yaml::gcp:cloudrun/service:Service::b",
+            "olds": {
+                "__meta": "{\"e2bfb730-ecaa-11e6-8f88-34363bc7c4c0\":{\"create\":1200000000000,\"delete\":1200000000000,\"update\":1200000000000},\"schema_version\":\"2\"}",
+                "autogenerateRevisionName": false,
+                "id": "locations/us-central1/namespaces/pulumi-development/services/b-476c751",
+                "location": "us-central1",
+                "metadata": {
+                    "annotations": {},
+                    "effectiveAnnotations": {
+                        "run.googleapis.com/ingress": "all",
+                        "run.googleapis.com/ingress-status": "all",
+                        "run.googleapis.com/operation-id": "bd6f4ceb-4105-4aac-a48b-2ab3ee98440f",
+                        "serving.knative.dev/creator": "ian@pulumi.com",
+                        "serving.knative.dev/lastModifier": "ian@pulumi.com"
+                    },
+                    "effectiveLabels": {
+                        "cloud.googleapis.com/location": "us-central1",
+                        "hi": "value"
+                    },
+                    "generation": 1,
+                    "labels": {},
+                    "namespace": "pulumi-development",
+                    "pulumiLabels": {
+                        "hi": "value"
+                    },
+                    "resourceVersion": "AAYTd5Ym10g",
+                    "selfLink": "/apis/serving.knative.dev/v1/namespaces/921927215178/services/b-476c751",
+                    "uid": "de263b8f-bb83-457f-9cb9-fe0e04657a6a"
+                },
+                "name": "b-476c751",
+                "project": "pulumi-development",
+                "statuses": [
+                    {
+                        "conditions": [
+                            {
+                                "message": "",
+                                "reason": "",
+                                "status": "True",
+                                "type": "Ready"
+                            },
+                            {
+                                "message": "",
+                                "reason": "",
+                                "status": "True",
+                                "type": "ConfigurationsReady"
+                            },
+                            {
+                                "message": "",
+                                "reason": "",
+                                "status": "True",
+                                "type": "RoutesReady"
+                            }
+                        ],
+                        "latestCreatedRevisionName": "b-476c751-00001-rbp",
+                        "latestReadyRevisionName": "b-476c751-00001-rbp",
+                        "observedGeneration": 1,
+                        "traffics": [
+                            {
+                                "latestRevision": true,
+                                "percent": 100,
+                                "revisionName": "b-476c751-00001-rbp",
+                                "tag": "",
+                                "url": ""
+                            }
+                        ],
+                        "url": "https://b-476c751-ctnulmzwoa-uc.a.run.app"
+                    }
+                ],
+                "template": {
+                    "metadata": {
+                        "annotations": {
+                            "autoscaling.knative.dev/maxScale": "100",
+                            "run.googleapis.com/cloudsql-instances": "instance:aeoi:zoi"
+                        },
+                        "generation": 0,
+                        "labels": {
+                            "run.googleapis.com/startupProbeType": "Default"
+                        },
+                        "name": "",
+                        "namespace": "",
+                        "resourceVersion": "",
+                        "selfLink": "",
+                        "uid": ""
+                    },
+                    "spec": {
+                        "containerConcurrency": 80,
+                        "containers": [
+                            {
+                                "args": [],
+                                "commands": [],
+                                "envFroms": [],
+                                "envs": [],
+                                "image": "gcr.io/cloudrun/hello",
+                                "livenessProbe": null,
+                                "name": "",
+                                "ports": [
+                                    {
+                                        "containerPort": 8080,
+                                        "name": "http1",
+                                        "protocol": ""
+                                    }
+                                ],
+                                "resources": {
+                                    "limits": {
+                                        "cpu": "1000m",
+                                        "memory": "512Mi"
+                                    },
+                                    "requests": {}
+                                },
+                                "startupProbe": {
+                                    "failureThreshold": 1,
+                                    "grpc": null,
+                                    "httpGet": null,
+                                    "initialDelaySeconds": 0,
+                                    "periodSeconds": 240,
+                                    "tcpSocket": {
+                                        "port": 8080
+                                    },
+                                    "timeoutSeconds": 240
+                                },
+                                "volumeMounts": [],
+                                "workingDir": ""
+                            }
+                        ],
+                        "serviceAccountName": "921927215178-compute@developer.gserviceaccount.com",
+                        "servingState": "",
+                        "timeoutSeconds": 300,
+                        "volumes": []
+                    }
+                },
+                "traffics": [
+                    {
+                        "latestRevision": true,
+                        "percent": 100,
+                        "revisionName": "",
+                        "tag": "",
+                        "url": ""
+                    }
+                ]
+            },
+            "news": {
+                "__defaults": [
+                    "autogenerateRevisionName",
+                    "name"
+                ],
+                "autogenerateRevisionName": false,
+                "location": "us-central1",
+                "name": "b-476c751",
+                "template": {
+                    "__defaults": [],
+                    "metadata": {
+                        "__defaults": [],
+                        "annotations": {
+                            "run.googleapis.com/cloudsql-instances": "instance:aeoi:zoi"
+                        }
+                    },
+                    "spec": {
+                        "__defaults": [],
+                        "containers": [
+                            {
+                                "__defaults": [],
+                                "image": "gcr.io/cloudrun/hello"
+                            }
+                        ]
+                    }
+                },
+                "traffics": [
+                    {
+                        "__defaults": [],
+                        "latestRevision": true,
+                        "percent": 100
+                    }
+                ]
+            },
+            "oldInputs": {
+                "__defaults": [
+                    "autogenerateRevisionName",
+                    "name"
+                ],
+                "autogenerateRevisionName": false,
+                "location": "us-central1",
+                "name": "b-476c751",
+                "template": {
+                    "__defaults": [],
+                    "metadata": {
+                        "__defaults": [],
+                        "annotations": {
+                            "run.googleapis.com/cloudsql-instances": "instance:aeoi:zoi"
+                        }
+                    },
+                    "spec": {
+                        "__defaults": [],
+                        "containers": [
+                            {
+                                "__defaults": [],
+                                "image": "gcr.io/cloudrun/hello"
+                            }
+                        ]
+                    }
+                },
+                "traffics": [
+                    {
+                        "__defaults": [],
+                        "latestRevision": true,
+                        "percent": 100
+                    }
+                ]
+            }
+        },
+        "response": {
+            "hasDetailedDiff": true,
+            "stables": "*",
+            "changes": "DIFF_SOME",
+            "diffs": [
+                "template"
+            ],
+            "detailedDiff": {
+                "template.metadata.annotations": {
+                    "kind": "UPDATE"
+                }
+            }
+        },
+        "metadata": {
+            "kind": "resource",
+            "mode": "client",
+            "name": "gcp"
+        }
+    }
+]`)
 }
