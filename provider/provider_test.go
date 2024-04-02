bbf613ec41095066dadc7ce91001713c68b0db59
Venelin <venelin@pulumi.com>
2024-04-02 17:00:24 +0100

    refactor upgrade test interface

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/pulumi/providertest/pulumitest/assertpreview"
 	"github.com/pulumi/providertest/pulumitest/opttest"
 	pfbridge "github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 
 	"github.com/pulumi/pulumi-gcp/provider/v7/pkg/version"
@@ -39,11 +40,39 @@ func TestUpgradeCoverage(t *testing.T) {
 	providertest.ReportUpgradeCoverage(t)
 }
 
-func testProviderUpgrade(t *testing.T, dir, baselineVersion string) {
-	testProviderUpgradeWithConfig(t, dir, baselineVersion, nil)
+type UpgradeTestOpts struct {
+	baselineVersion string
+	assertFunc      func(*testing.T, auto.PreviewResult)
+	config          map[string]string
 }
 
-func testProviderUpgradeWithConfig(t *testing.T, dir, baselineVersion string, config map[string]string) {
+func WithBaselineVersion(baselineVersion string) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.baselineVersion = baselineVersion
+	}
+}
+
+func WithAssertFunc(assertFunc func(*testing.T, auto.PreviewResult)) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.assertFunc = assertFunc
+	}
+}
+
+func WithConfig(config map[string]string) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.config = config
+	}
+}
+
+func testProviderUpgrade(t *testing.T, dir string, opts ...func(*UpgradeTestOpts)) {
+	options := &UpgradeTestOpts{}
+	for _, o := range opts {
+		o(options)
+	}
+	testProviderUpgradeWithConfig(t, dir, options.baselineVersion, options.config, options.assertFunc)
+}
+
+func testProviderUpgradeWithConfig(t *testing.T, dir, baselineVersion string, config map[string]string, assertFunction func(*testing.T, auto.PreviewResult)) {
 	if testing.Short() {
 		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without GCP creds")
 	}
@@ -63,7 +92,11 @@ func testProviderUpgradeWithConfig(t *testing.T, dir, baselineVersion string, co
 	// The SetConfig above does not seem to be working here.
 	t.Setenv("PULUMI_GCP_SKIP_REGION_VALIDATION", "true")
 	result := providertest.PreviewProviderUpgrade(test, providerName, baselineVersion, optproviderupgrade.DisableAttach())
-	assertpreview.HasNoReplacements(t, result)
+	if assertFunction != nil {
+		assertFunction(t, result)
+    } else {
+		assertpreview.HasNoReplacements(t, result)
+	}
 }
 
 func providerServer(t *testing.T) pulumirpc.ResourceProviderServer {
