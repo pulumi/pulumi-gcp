32b0f5741fb8c0b6af363550c3c9bc1799b92b0c
Venelin <venelin@pulumi.com>
2024-04-26 12:30:23 +0100

    make token secret again

--- sdk/java/src/main/java/com/pulumi/gcp/Provider.java
+++ sdk/java/src/main/java/com/pulumi/gcp/Provider.java
@@ -10,6 +10,7 @@ import com.pulumi.core.internal.Codegen;
 import com.pulumi.gcp.ProviderArgs;
 import com.pulumi.gcp.Utilities;
 import java.lang.String;
+import java.util.List;
 import java.util.Optional;
 import javax.annotation.Nullable;
 
@@ -1017,6 +1018,9 @@ public class Provider extends com.pulumi.resources.ProviderResource {
     private static com.pulumi.resources.CustomResourceOptions makeResourceOptions(@Nullable com.pulumi.resources.CustomResourceOptions options, @Nullable Output<String> id) {
         var defaultOptions = com.pulumi.resources.CustomResourceOptions.builder()
             .version(Utilities.getVersion())
+            .additionalSecretOutputs(List.of(
+                "accessToken"
+            ))
             .build();
         return com.pulumi.resources.CustomResourceOptions.merge(defaultOptions, options, id);
     }
