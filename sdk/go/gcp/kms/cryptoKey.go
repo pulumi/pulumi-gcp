29354985dacfe97bad8eea7ade3e50948adf9e33
guineveresaenger <guinevere@pulumi.com>
2024-06-18 13:04:16 -0700

    make sdks

--- sdk/go/gcp/kms/cryptoKey.go
+++ sdk/go/gcp/kms/cryptoKey.go
@@ -19,9 +19,7 @@ import (
 // and delete all CryptoKeyVersions, rendering the key unusable, but *will
 // not delete the resource from the project.* When the provider destroys these keys,
 // any data previously encrypted with these keys will be irrecoverable.
-// For this reason, it is strongly recommended that you add
-// lifecycle
-// hooks to the resource to prevent accidental destruction.
+// For this reason, it is strongly recommended that you use the [pulumi protect resource option](https://www.pulumi.com/docs/concepts/options/protect/)
 //
 // To get more information about CryptoKey, see:
 //
--- sdk/python/pulumi_gcp/kms/crypto_key.py
+++ sdk/python/pulumi_gcp/kms/crypto_key.py
@@ -498,9 +498,7 @@ class CryptoKey(pulumi.CustomResource):
         and delete all CryptoKeyVersions, rendering the key unusable, but *will
         not delete the resource from the project.* When the provider destroys these keys,
         any data previously encrypted with these keys will be irrecoverable.
-        For this reason, it is strongly recommended that you add
-        lifecycle
-        hooks to the resource to prevent accidental destruction.
+        For this reason, it is strongly recommended that you use the [pulumi protect resource option](https://www.pulumi.com/docs/concepts/options/protect/)
 
         To get more information about CryptoKey, see:
 
@@ -605,9 +603,7 @@ class CryptoKey(pulumi.CustomResource):
         and delete all CryptoKeyVersions, rendering the key unusable, but *will
         not delete the resource from the project.* When the provider destroys these keys,
         any data previously encrypted with these keys will be irrecoverable.
-        For this reason, it is strongly recommended that you add
-        lifecycle
-        hooks to the resource to prevent accidental destruction.
+        For this reason, it is strongly recommended that you use the [pulumi protect resource option](https://www.pulumi.com/docs/concepts/options/protect/)
 
         To get more information about CryptoKey, see:
 
