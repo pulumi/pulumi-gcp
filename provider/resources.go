7116914a4fcf71ce1fbd83161913c655a881b699
Alberto Pose <albertopose@gmail.com>
2026-08-05 10:32:06 +0100

    make tfgen

--- provider/resources.go
+++ provider/resources.go
@@ -179,6 +179,7 @@ var moduleMapping = map[string]string{
 	"access_approval":                 gcpAccessApproval,
 	"access_context_manager":          gcpAccessContextManager,
 	"active_directory":                gcpActiveDirectory,
+	"agent_identity":                  "AgentIdentity",
 	"agent_registry":                  "AgentRegistry",
 	"alloydb":                         gcpAlloydb,
 	"api_gateway":                     gcpApiGateway,
